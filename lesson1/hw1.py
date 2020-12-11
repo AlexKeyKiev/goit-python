2ade04a1b37aca092232f4a28836545fa6188e4b
AlexKeyKiev <75761637+AlexKeyKiev@users.noreply.github.com>
2020-12-11 05:13:00 +0200

    Update hw1.py

--- lesson1/hw1.py
+++ lesson1/hw1.py
@@ -1,4 +1,4 @@
-user_name = input ('Enter your name please ')
+user_name = input ('Enter your name please: ')
 privetstvie = f"Hello, {user_name}!"
 print (privetstvie)
 input ("Push 'Enter' to exit")
